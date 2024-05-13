0dbdfed8308fc55b4eec56b9a18f0864afb642ad
David Perez Moreno <davidperezmoreno14@gmail.com>
2024-05-13 16:37:58 +0200

    Implementacion de la funcionalidad del formato HTML

--- src/main/java/ubu/gii/Customer.java
+++ src/main/java/ubu/gii/Customer.java
@@ -34,7 +34,11 @@ public class Customer {
 		double totalAmount = 0;
 		int frequentRenterPoints = 0;
 		Iterator<Rental> rentals = _rentals.iterator();
-		String result = "Rental Record for " + getName() + "\n";
+		StringBuilder result=new StringBuilder();
+		result.append("<html><head><title>Rental Record</title></head><body>");
+        result.append("<h1>Rental Record for " + getName() + "</h1>");
+        result.append("<table>");
+        result.append("<tr><th>Title</th><th>Amount</th></tr>");
 		while (rentals.hasNext()) {
 			double thisAmount = 0;
 			Rental each = rentals.next();
@@ -48,15 +52,15 @@ public class Customer {
 					&& each.getDaysRented() > 1)
 				frequentRenterPoints++;
 			// show figures for this rental
-			result += "\t" + each.getMovie().getTitle() + "\t"
-					+ String.valueOf(thisAmount) + "\n";
+			result.append("<tr><td>" + each.getMovie().getTitle() + "</td><td>" + String.format("%.2f", thisAmount) + "</td></tr>");
 			totalAmount += thisAmount;
 		}
 		// add footer lines
-		result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
-		result += "You earned " + String.valueOf(frequentRenterPoints)
-				+ " frequent renter points";
-		return result;
+		result.append("</table>");
+        result.append("<p>Amount owed is " + String.format("%.2f", totalAmount) + "</p>");
+        result.append("<p>You earned " + frequentRenterPoints + " frequent renter points</p>");
+        result.append("</body></html>");
+		return result.toString();
 	}
 
 	private double amountFor(Rental each) {
