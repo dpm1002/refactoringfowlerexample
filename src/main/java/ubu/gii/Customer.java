f736bff2d6c4564e2ad070680e5f3ab11b34d255
Malonsoalonso <maa1039@alu.ubu.es>
2024-05-16 12:35:13 +0200

    Hemos echo un refactor de move method del metodo getCharge a la clase Rental

--- src/main/java/ubu/gii/Customer.java
+++ src/main/java/ubu/gii/Customer.java
@@ -62,23 +62,10 @@ public class Customer {
 		return result.toString();
 	}
 
+	/**
+	 * @deprecated Use {@link ubu.gii.Rental#getCharge()} instead
+	 */
 	private double getCharge(Rental aRental) {
-		double thisAmount = 0;
-		switch (aRental.getMovie().getPriceCode()) {
-		case Movie.REGULAR:
-			thisAmount += 2;
-			if (aRental.getDaysRented() > 2)
-				thisAmount += (aRental.getDaysRented() - 2) * 1.5;
-			break;
-		case Movie.NEW_RELEASE:
-			thisAmount += aRental.getDaysRented() * 3;
-			break;
-		case Movie.CHILDRENS:
-			thisAmount += 1.5;
-			if (aRental.getDaysRented() > 3)
-				thisAmount += (aRental.getDaysRented() - 3) * 1.5;
-			break;
-		}
-		return thisAmount;
+		return aRental.getCharge();
 	}
 }
