97865e66314a0d1fcecc12d837b972e8746c7450
David Perez Moreno <davidperezmoreno14@gmail.com>
2024-05-16 13:47:30 +0200

    Hemos movido con el refactor move method el metodo de getCharge a Prize

--- src/main/java/ubu/gii/Movie.java
+++ src/main/java/ubu/gii/Movie.java
@@ -45,23 +45,7 @@ public class Movie {
 
 	
 	public double getCharge(Rental rental) {
-		double thisAmount = 0;
-		switch (rental.getMovie().getPriceCode()) {
-		case 0:
-			thisAmount += 2;
-			if (rental.getDaysRented() > 2)
-				thisAmount += (rental.getDaysRented() - 2) * 1.5;
-			break;
-		case 1:
-			thisAmount += rental.getDaysRented() * 3;
-			break;
-		case 2:
-			thisAmount += 1.5;
-			if (rental.getDaysRented() > 3)
-				thisAmount += (rental.getDaysRented() - 3) * 1.5;
-			break;
-		}
-		return thisAmount;
+		return _priceCode.getCharge(rental);
 	}
 
 	public int getFrequentPoint(Rental rental) {
--- src/main/java/ubu/gii/Prize.java
+++ src/main/java/ubu/gii/Prize.java
@@ -4,4 +4,24 @@ public abstract class Prize {
 
 	public abstract int getPriceCode();
 
+	public double getCharge(Rental rental) {
+		double thisAmount = 0;
+		switch (rental.getMovie().getPriceCode()) {
+		case 0:
+			thisAmount += 2;
+			if (rental.getDaysRented() > 2)
+				thisAmount += (rental.getDaysRented() - 2) * 1.5;
+			break;
+		case 1:
+			thisAmount += rental.getDaysRented() * 3;
+			break;
+		case 2:
+			thisAmount += 1.5;
+			if (rental.getDaysRented() > 3)
+				thisAmount += (rental.getDaysRented() - 3) * 1.5;
+			break;
+		}
+		return thisAmount;
+	}
+
 }
